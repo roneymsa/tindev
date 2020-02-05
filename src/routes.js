3300ecd6ab2a208a7e5c61c90c47fbb543688d06
roney-mendes <roneymendes1@gmail.com>
2020-02-04 22:52:53 -0300

    Versão estável das rotas com Switch Navigator

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
-import { createAppContainer, createSwichNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
 
-export default createAppContainer(
+const Routes = createAppContainer(
     createSwitchNavigator({
-        Login,
-        Main,
+        Login: {
+            screen: Login,
+            navigationOptions: {
+                title: 'Login Tindev'
+            },
+        },
+        Main: {
+            screen: Main,
+            navigationOptions: {
+                title: 'Tindev'
+            },
+        },
     })
 );
+
+export default Routes;
